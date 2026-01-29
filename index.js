78b5dc5b33cb10c6f096eca28c26b714fcf3b794
AmirDihan <amirhossan160@gmail.com>
2026-01-29 23:06:13 +0600

    GET api endpoint imlemented for all services page with dotenv config

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("server is running fine")
-})
+const uri =
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zbdwt9q.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("HomeFixo");
+    const services = db.collection("services");
+
+    // GET: Api endpoint for fetching all the services data from mongodb
+    app.get("/services", async (req, res) => {
+      const result = await services.find().toArray();
+      res.send(result);
+    });
+
+    //POST: Api endpoint to add a new service to mongodb
+    app.post("/service", async (req, res) => {
+      const data = req.body;
+      const result = await services.insertOne();
+
+      res.send({
+        success: true,
+        message: "service added successfully!",
+      });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running fine");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
